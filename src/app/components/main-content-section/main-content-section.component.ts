d4eec46d23a45c55eca3ddad849aaf64d8eb40ad
Luis.souza03 <luis93667@gmail.com>
2022-09-09 18:39:45 -0300

    feat: change buttons and add read more

--- src/app/components/main-content-section/main-content-section.component.ts
+++ src/app/components/main-content-section/main-content-section.component.ts
@@ -1,6 +1,8 @@
 import { ITmdbMovieData, ITmdbResponse } from '../../models/tmdb.model';
 import { TmdbService } from '../../services/tmdb.service';
 import { Component, OnInit } from '@angular/core';
+import { InfoMovieModalComponent } from 'src/app/shared/components/info-movie-modal/info-movie-modal.component';
+import { MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
 
 
 @Component({
@@ -12,8 +14,13 @@ export class MainContentSectionComponent implements OnInit {
   imgBackground!: string;
   movieTrending!: ITmdbMovieData;
 
+  modalRef: MdbModalRef<InfoMovieModalComponent> | null = null;
+
+  isReadMore = true
+
   constructor(
-    private tmdbService: TmdbService
+    private tmdbService: TmdbService,
+    private modalService: MdbModalService
   ) {
     this.tmdbService.getTrendingMovies().subscribe((data: ITmdbResponse) => {
 
@@ -27,9 +34,21 @@ export class MainContentSectionComponent implements OnInit {
   }
 
   getOneRandomMovie(moviesData: Array<ITmdbMovieData>): void {
+    debugger
     this.movieTrending = moviesData[Math.floor(Math.random() * 19)] as ITmdbMovieData;
 
     this.imgBackground = 'https://image.tmdb.org/t/p/original/' + this.movieTrending.backdrop_path;
+  }
+
+  openModal(movieSelect: any) {
+    this.modalRef = this.modalService.open(InfoMovieModalComponent, {
+      modalClass: 'modal-dialog',
+      data: movieSelect
+    })
+  }
+
+  showText() {
+    this.isReadMore = !this.isReadMore
   }
 
 }
