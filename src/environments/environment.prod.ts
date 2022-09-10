d383b8dd4c5fc7e230716d6a1e5cb9672ff73a59
Luis.souza03 <luis93667@gmail.com>
2022-09-10 17:38:13 -0300

    test: test build on github page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: false,
+
+  API_KEY: "3f8972ff48c327edd722fe5c343f29d6",
+  API_URL: "https://api.themoviedb.org/3",
+  api: "http://localhost:3000",
 };
