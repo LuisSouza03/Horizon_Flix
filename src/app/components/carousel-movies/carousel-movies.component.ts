46a6b4c19926fb2690ae82e666fe719dd69d37cc
Luis.souza03 <luis93667@gmail.com>
2022-09-12 21:57:21 -0300

    feat: add new genre in carousel movies

--- src/app/components/carousel-movies/carousel-movies.component.ts
+++ src/app/components/carousel-movies/carousel-movies.component.ts
@@ -1,7 +1,6 @@
 import { ITmdbMovieData, ITmdbResponse } from '../../models/tmdb.model';
 import { Component, OnInit } from '@angular/core';
 import { TmdbService } from 'src/app/services/tmdb.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-carousel-movies',
@@ -10,19 +9,23 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class CarouselMoviesComponent implements OnInit {
 
-  titleSection = ['Em Alta', 'Originais HorizonFlix', 'Terror']
-  popularMovies!: Array<ITmdbMovieData>;
-  originalsMovies!: Array<ITmdbMovieData>;
-  horrorMovies!: Array<ITmdbMovieData>;
+  titleSection = ['Em Alta', 'Originais HorizonFlix', 'Terror e Suspense', 'Ação e Aventura']
+
+  popularMovies: Array<ITmdbMovieData>;
+  originalsMovies: Array<ITmdbMovieData>;
+  horrorMovies: Array<ITmdbMovieData>;
+  actionMovies: Array<ITmdbMovieData>;
 
   constructor(
     private tmdbService: TmdbService,
   ) { }
 
   ngOnInit(): void {
+    debugger
     this.getPopularMovies();
     this.getOriginalsMovies();
     this.getHorrorMovies();
+    this.getActionAndAdventureMovies();
   }
 
   getPopularMovies() {
@@ -50,10 +53,23 @@ export class CarouselMoviesComponent implements OnInit {
   }
 
   getHorrorMovies() {
+    let horrorMoviesData: any;
+    let thrillerMoviesData: any;
+
     this.tmdbService.getMoviesForCategory('27').subscribe((data: ITmdbResponse) => {
-      const horrorMoviesData: Array<ITmdbMovieData> = data.results;
+      horrorMoviesData = data.results;
 
       this.horrorMovies = horrorMoviesData;
+    })
+
+    this.tmdbService.getMoviesForCategory('53').subscribe((data: ITmdbResponse) => {
+      thrillerMoviesData = data.results;
+
+      for (let i in horrorMoviesData) {
+        thrillerMoviesData.push(horrorMoviesData[i]);
+      }
+
+      this.horrorMovies = thrillerMoviesData;
 
       for (let i in this.horrorMovies) {
         this.horrorMovies[i].poster_path = `https://image.tmdb.org/t/p/original/${this.horrorMovies[i].poster_path}`;
@@ -61,4 +77,28 @@ export class CarouselMoviesComponent implements OnInit {
     })
   }
 
+  getActionAndAdventureMovies() {
+    let actionMoviesData: any;
+    let adventureMoviesData: any;
+
+    this.tmdbService.getMoviesForCategory('28').subscribe((data: ITmdbResponse) => {
+      actionMoviesData = data.results;
+
+      this.actionMovies = actionMoviesData;
+    })
+
+    this.tmdbService.getMoviesForCategory('12').subscribe((data: ITmdbResponse) => {
+      adventureMoviesData = data.results;
+
+      for (let i in actionMoviesData) {
+        adventureMoviesData.push(actionMoviesData[i])
+      }
+
+      this.actionMovies = adventureMoviesData;
+
+      for (let i in this.actionMovies) {
+        this.actionMovies[i].poster_path = `https://image.tmdb.org/t/p/original/${this.actionMovies[i].poster_path}`;
+      }
+    })
+  }
 }
